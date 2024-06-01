7200f1c8aec8c00465447163d10bb0c8ba8816ac
Visilv <victor7.ramos12@gmail.com>
2024-06-01 11:47:03 -0300

    última att do desafio.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,26 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    private int numero;
-    private String agencia;
-    private String nomeCliente;
-    private double saldo;
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+    
+        System.out.println("Por favor, digite o número da agência: ");
+        int agencia = sc.nextInt();
 
-    public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
-        this.numero = numero;
-        this.agencia = agencia;
-        this.nomeCliente = nomeCliente;
-        this.saldo = saldo;
-    }
+        System.out.println("Por favor, digite o número da conta: ");
+        String conta = sc.next();
 
-    public int getNumero() {
-        return numero;
-    }
-    public String getAgencia() {
-        return agencia;
-    }
-    public String getNomeCliente() {
-        return nomeCliente;
-    }
-    public double getSaldo() {
-        return saldo;
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o valor de depósito: ");
+        Double saldo = sc.nextDouble();
+
+        sc.close();
+        
+        System.out.println("Olá " + nomeCliente + 
+        ", obrigado por criar uma conta em nosso banco, sua agência é "
+         + agencia + ", conta " + conta + " e seu saldo R$" + saldo +" já está disponível para saque.");
     }
 }
